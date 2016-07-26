acedd86f8fe4cd145d701c29c399809dd8b5552b
WalissonPires <walissonpiresgv@hotmail.com>
2016-07-26 18:19:48 -0300

    Implementação do Chat
    Implementação do chat com um ChatBot externo.
    http://www.personalityforge.com/

---
+++ whatsapp/js/app.js
@@ -0,0 +1,116 @@
+/* 
+	Formato da mensagem ---------
+
+		Mensagem {
+			usuario,
+			texto,
+			horario,
+			emocao
+		}
+
+	-----------------------------
+}
+*/
+
+/*  -----  Inicialização ------  */
+
+$('#input-mensagem').keyup(function (e) {
+
+	if ((e.keyCode == 13) && ($('#input-mensagem').val() != ""))  {
+		enviarMensagem($('#input-mensagem').val());
+		return false;
+	}
+});
+
+
+
+/*  ----- Funções de apoio ----- */
+
+function falar(texto, callbackSuccess, callbackError) {
+
+	texto = encodeURIComponent(texto);
+
+	$.ajax({
+		url: 'http://www.personalityforge.com/api/chat/?apiKey=jDc2JqWuADdbXLO7&chatBotID=6&message=' + texto + '&externalID=abc-639184572&firstName=Tugger&lastName=Sufani&gender=m',
+		method: 'GET',
+		success: function (resposta) {
+
+			resposta = JSON.parse(resposta);
+
+			if (resposta.success != 1) {
+
+				if (callbackError != undefined)
+					callbackError(resposta.errorMessage);
+			}
+			else {
+
+				var data = new Date();
+				var horarioAtual = data.getHours() + ':' + data.getMinutes();
+
+				callbackSuccess({
+					usuario: resposta.message.chatBotName,
+					texto: resposta.message.message,
+					emocao: resposta.message.emotion,
+					horario: horarioAtual
+				});
+			}
+		}, 
+		error: callbackError
+	});
+};
+
+function addMensagemEsqueda(mensagem) {
+
+	var msg = $('<div class="chat-mensagem"><div class="chat-mensagem-ponta mensagem-esquerda-align"><img src="img/icone-balao-esquerda.png" alt="" class=""></div><div class="chat-mensagem-texto mensagem-esquerda-align mensagem-esquerda-background"><p name="mensagem-texto-conteudo"></p><div class="mensagem-texto-status"><p name="chat-mensagem-horario"></p><img src="img/icone-msg-visualizada.png" alt="" class="chat-mensagem-visualiza"></div></div></div>');
+
+	$('[name="mensagem-texto-conteudo"]', msg).html(mensagem.texto);
+	$('[name="chat-mensagem-horario"]', msg).html(mensagem.horario);
+
+	$(".area-chat").append(msg);
+}
+
+function addMensagemDireita(mensagem) {
+
+	var msg = $('<div class="chat-mensagem"><div class="chat-mensagem-ponta mensagem-direita-align"><img src="img/icone-balao-direita.png" alt="" class=""></div><div class="chat-mensagem-texto mensagem-direita-align mensagem-direita-background"><p name="mensagem-texto-conteudo"></p><div class="mensagem-texto-status"><p name="chat-mensagem-horario"></p><img src="img/icone-msg-visualizada.png" alt="" class="chat-mensagem-visualiza"></div></div></div>');
+
+	$('[name="mensagem-texto-conteudo"]', msg).html(mensagem.texto);
+	$('[name="chat-mensagem-horario"]', msg).html(mensagem.horario);
+
+	$(".area-chat").append(msg);
+}
+
+function enviarMensagem(texto) {
+
+	var data = new Date();
+	var horarioAtual = data.getHours() + ':' + data.getMinutes();
+
+	addMensagemDireita({
+		texto: texto, 
+		horario: horarioAtual, 
+		emotion: undefined, 
+		usuario: undefined
+	});
+
+	rolarScroll();
+
+	$('#input-mensagem').val("");
+
+	falar(
+		texto, 
+		function(resposta) {
+			addMensagemEsqueda(resposta);
+
+			rolarScroll();
+		},
+		function(erro) {
+			alert(erro);
+		}
+	);
+}
+
+
+function rolarScroll() {
+
+	var container = document.getElementsByClassName("area-chat")[0];
+	container.scrollTop = container.scrollHeight;
+}
